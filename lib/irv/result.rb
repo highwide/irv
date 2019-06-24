670d88e3d4b5d358ee15f23e6af89e26c291259f
highwide <hochweit728@gmail.com>
2019-06-25 02:32:37 +0900

    Fix inifinite loop

--- lib/irv/result.rb
+++ lib/irv/result.rb
@@ -10,12 +10,14 @@ module Irv
       @process = []
       round_order = 1
       round = Round.new(round_order, candidates, ballots.map(&:ranked_candidates))
-      while !round.exist_majority?
+
+      (candidates.count - 1).times do
         @process << round
+        break if round.exist_majority?
+
         round_order += 1
         round = Round.new(round_order, round.next_candidates, round.next_votes)
       end
-      @process << round
     end
 
     def winner
